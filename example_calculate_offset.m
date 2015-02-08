95e562f29a4f4df655a109d4930e8a0c57ff2329
Jia-Shen Boon <jiashen@gmail.com>
2015-02-08 10:21:59 -0600

    Test MTB on two images of different exposures

--- example_calculate_offset.m
+++ example_calculate_offset.m
@@ -1,15 +1,16 @@
 %% Align two offset images
 
-filename = 'exposures/img08.jpg';
-max_offset = 100;  % Assume offset in any dim is always less than this
+% Load two grayscale images of difference exposures
+im_exp1 = rgb2gray(imread('exposures/img08.jpg'));
+im_exp2 = rgb2gray(imread('exposures/img05.jpg'));
 
-im_ref = rgb2gray(imread(filename));
+im_ref = im_exp1;  % Reference image for alignment (and we shift the other one)
 
 for i = 1:3
     
-    % Translate reference image by a ground truth
+    % Translate second image by a ground truth
     true_offset = randi([-max_offset max_offset], [1 2]);
-    im_offset = circshift(im_ref, true_offset);
+    im_offset = circshift(im_exp2, true_offset);
 
     % Calculate offset
     calc_offset = calculate_offset(im_ref, im_offset, max_offset);
@@ -19,7 +20,4 @@ for i = 1:3
     subplot(1, 2, 1); imshow(im_ref); title('Reference image');
     subplot(1, 2, 2); imshow(im_offset); title(['Offset image, ' figure_title]);
     
-    % Compare calculation with truth
-    fprintf('%s \t True offset\n', num2str(true_offset));
-    fprintf('%s \t Calculated offset\n', num2str(calc_offset));
 end
