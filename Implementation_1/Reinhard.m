5e803098b6a72626e0c8c22245e0c8aa018ef4d9
Jia-Shen Boon <jiashen@gmail.com>
2015-02-12 12:07:49 -0600

    Fix: Reinhart unused args, overwriting args
    Previously Reinhart had phi and epsilon unused args. We remove them.
    
    Previously Reinhart overwrote user-supplied args in the first few lines of the
    function. We remove this part so now user-supplied args actually make a
    difference.

--- Implementation_1/Reinhard.m
+++ Implementation_1/Reinhard.m
@@ -4,24 +4,20 @@
 %
 % input:
 %   img: 3 channel HDR img
-%   type_: 'global'(default) or 'local'.
 %   alpha_: scalar constant to specify a high key or low key. (0.18)
 %   delta: scalar constant to prevent log(0). (1e-6)
 %   white_: scalar constant, the smallest luminance to be mapped to 1. (1.5)
-%   phi: (local) scalar constant. (4)
-%   epsilon: (local) scalar constant to tell the terminating threshold. (1e-4)
-%
 % output:
 %   tone-mapped image (LDR or SDR)
+
+% unused input:
+%   phi: (local) scalar constant. (4)
+%   type_: 'global'(default) or 'local'.
+%   epsilon: (local) scalar constant to tell the terminating threshold. (1e-4)
+%
 %
 
-function imgOut = Reinhard(img, alpha_, delta, white_, phi, epsilon)
-
-alpha_ = 0.18;
-delta = 1e-6;
-white_ = 1.5;
-phi = 4;
-epsilon = 1e-4;
+function imgOut = Reinhard(img, alpha_, delta, white_)
 
 imgOut = zeros(size(img));
 
